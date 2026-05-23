9fb3e0aad3ee37faf9de7db42d20d27e68e4f34f
AyayronSu <aarondsujana@gmail.com>
2026-05-23 13:01:23 -0500

    Configured logging and registered blueprint in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,31 @@
 import os
+import logging
+from logging.config import dictConfig
 from flask import Flask
 from flask_cors import CORS
 from extensions import db, login_manager
 from models.user import User
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',        
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default'
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    }
+})
 
 def create_app():
     app = Flask(__name__)
