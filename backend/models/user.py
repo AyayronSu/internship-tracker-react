176d974ece0a2299943391626de37367cf10e1fb
AyayronSu <aarondsujana@gmail.com>
2026-05-23 11:56:46 -0500

    Updated user.py

---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+import uuid
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    applications = db.relationship('Application', backref='owner', lazy=True, cascade="all, delete-orphan")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
